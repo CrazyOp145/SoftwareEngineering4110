f7b8c8e9904fd63fcd9239f021354c038d517f91
David Her <42558414+R3aLyze@users.noreply.github.com>
2022-11-01 22:17:15 -0400

    Add Project File

---
+++ CSC4110-GroupProject/src/Profiles.java
@@ -0,0 +1,38 @@
+public abstract class Profiles {
+    private String firstName;
+    private String lastName;
+    private String ID;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
