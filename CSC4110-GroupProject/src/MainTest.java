1c559f39148e89c215c00c03ab0c40659a54de2b
Mahz, Alam <74alamm@gmail.com>
2022-11-30 12:33:38 -0500

    Updated Maint Test Class
    - Made the character count system work more fluently
    
    --Mahz Alam gw1637

--- CSC4110-GroupProject/src/MainTest.java
+++ CSC4110-GroupProject/src/MainTest.java
@@ -1,24 +1,31 @@
 import Profiles.LoginValidation;
-
+import Profiles.ProfilesFactory;
+import Profiles.UserProfiles;
 import java.util.Scanner;
 
 public class MainTest {
-    public static void main(String[] args){
-        LoginValidation.createUserProfiles();
+    public static void main(String[] args) {
+        //LoginValidation.createUserProfiles();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please input a name for the Vendor: ");
-        vName = scanner.nextLine();
+        String vName = scanner.nextLine();
         vName = characterLimit(20, vName);
         System.out.println("You inputted: " + vName);
-        
+
         String customer = "Customer Profile";
         UserProfiles profile1 = ProfilesFactory.createProfile(customer);
         System.out.println(profile1.toString());
     }
 
     public static String characterLimit(int limit, String stringToLimit) {
-        String cutName = stringToLimit.substring(0, limit);
         if (stringToLimit.length() > limit) {
+            String cutName = stringToLimit.substring(0, limit);
             stringToLimit = cutName;
+            return stringToLimit;
+        }
+        else{
+            return stringToLimit;
         }
     }
+}
