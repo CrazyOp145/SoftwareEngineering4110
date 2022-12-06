dcf2974461a638758fa613fdcc2b8dd6528dffd4
PauloBurgess0 <113396496+PauloBurgess0@users.noreply.github.com>
2022-12-06 17:28:18 -0500

    Added Delete Customer Profile and Linked it

--- CSC4110-GroupProject/src/UserMenuController.java
+++ CSC4110-GroupProject/src/UserMenuController.java
@@ -100,6 +100,14 @@ public class UserMenuController implements Initializable {
         Stage.setScene(Scene);
         Stage.show();
     }
+    public void switchDeleteCustomerProfile(javafx.event.ActionEvent event) throws IOException {
+        Root = FXMLLoader.load(getClass().getResource("DeleteCustomerProfile.fxml"));
+        //Stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage = (Stage)myMenuBar.getScene().getWindow();
+        Scene = new Scene(Root);
+        Stage.setScene(Scene);
+        Stage.show();
+    }
 
     public void switchToCreateUserProfile(javafx.event.ActionEvent event) throws IOException {
         Root = FXMLLoader.load(getClass().getResource("Profiles/Login/CreateUserProfile.fxml"));
